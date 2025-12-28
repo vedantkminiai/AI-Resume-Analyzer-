7dea283d3d4015f6e7d7e5a6c488c7ff9a3a5eba
vedantkminiai <vedant.kansara@student.tdsb.on.ca>
2025-12-28 06:01:26 -0500

    Added the navbar component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {resume} from "react-dom/server";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "EmployAI" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className={"main-section"}>
+      <div className="page-heading">
+        <h1>Track Your Applications and Resume Ratings & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+
+    {resumes.map(resume => (
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+    ))}
+
+  </main>
 }
