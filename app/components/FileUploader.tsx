fd4baa0dd468d600119d9457268c500cf7985abd
vedantkminiai <vedantkansara224@gmail.com>
2026-01-08 03:16:02 -0500

    Added styling to drag and drop button

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -37,14 +37,19 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                     </div>
 
                     {file ? (
-                        <div className={"text-center"}>
-                            <p className={"text-1g text-gray-700 font-medium truncate"}>
-                                {file.name}
-                            </p>
-                            <p className={"text-sm text-gray-500"}>
-                                {formatSize(file.size)}
-                            </p>
-                        </div>
+                            <div className={"uploader-selected-file"} onClick={(e) => e.stopPropagation()}>
+                                <img src={"/images/pdf.png"} alt={"pdf"} className={"size-10"} />
+                                <div className={"flex items-center space-x-3"}>
+                                    <div>
+                                        <p className={"text-sm font-medium text-gray-700 truncate max-w-xs"}>
+                                            {file.name}
+                                        </p>
+                                        <p className={"text-sm text-gray-500"}>
+                                            {formatSize(file.size)}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                     ) : (
                         <div>
                             <p className={"text-lg text-gray-500"}>
@@ -54,7 +59,8 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                             </p>
                             <p className={"text-lg text-gray-500"}>PDF (max  {formatSize(maxFileSize)})</p>
                         </div>
-                    )}
+                    )
+                    }
                 </div>
             </div>
         </div>
